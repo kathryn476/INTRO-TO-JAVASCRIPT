af452f37d8a3d09f5b6ffd68ea26fe9bb937b7ad
kathryn41 <ssebandekelawrencestifler@gmail.com>
2022-06-22 09:06:41 +0300

    PUSH

---
+++ index.js
@@ -0,0 +1,7 @@
+//javascript that prints out my name 
+let Name = "Catherine Nakamaanya";
+let height = 5.1;
+let country = "Uganda"
+console.log( `My name is ${Name}`) // using backticks and dollar sign to join 
+console.log(`My height is ${height}`)
+console.log(`I am from ${country}`)
